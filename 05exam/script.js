d698621b1014f0c99bcfed577df4401222659673
Yacine <simohammed.yacine@gmail.com>
2024-03-15 15:55:57 +0100

    fin 1er cours

--- 05exam/script.js
+++ 05exam/script.js
@@ -1,26 +1,32 @@
-function changeApi() { 
+ID=""
+let ApiUrl=`https://pokeapi.co/api/v2/pokemon/`
+
+function changeApi() {
     ID = document.getElementById("recherche").value;
     ApiUrl = `https://pokeapi.co/api/v2/pokemon/${ID}/`
 
 }
+
 fetch= ApiUrl;
+
+function GetApi() {
+let outputElement = document.getElementById('output');
 
 
 // Make a GET request
 fetch(ApiUrl)
   .then(response => {
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error('Error Network');
     }
     return response.json();
   })
-  .then(data => {
-    JSonMcData = data
-    mcData = JSON.stringify(JSonMcData);
-    outputElement.textContent =  mcData.online + ", " + mcData.ip + ", " + mcData.hostname + ", " + mcData.version + ", " + mcData.players.online
+  .then(pokemon => {
+    Pokedex = pokemon
+    Pokestr = JSON.stringify(Pokedex);
+    outputElement.textContent =  Pokestr.online + ", " + Pokestr.ip + ", " + Pokestr.hostname + ", " + Pokestr.version + ", " + Pokestr.players.online
   })
   .catch(error => {
     console.error('Error:', error);
   });
-
 }
